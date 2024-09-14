08e64b5243832fb8b661eb384b4e222238fc50e5
Luan Tran <devluant@gmail.com>
2024-09-14 22:40:52 +0700

    Render database data on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
-export default function HomePage() {
-    return (
-        <h1>HomePage</h1>
-    )
+import getLesson from "@/server/actions/get-lesson"
+
+export default async function HomePage() {
+    const { error, success }= await getLesson()
+    if(error) throw new Error(error)
+    
+    if (success) {
+        const lineElements = success.map(line => {
+            return (<li key={line.id}>{ line.title }</li>)
+        })
+
+        return (
+            <div>
+                <h1>HomePage</h1>
+                <ul>
+                    { lineElements }
+                </ul>
+            </div>
+        )
+    }
+
+
+        
 }
