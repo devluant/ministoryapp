434004e48f2c35daaadfdeb52690aa1287790c44
Luan Tran <devluant@gmail.com>
2024-09-15 09:07:38 +0700

    Import alert diaglogue to page Home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
-import "@/assets/styles/globals.css"
-import type { Metadata } from "next"
+import "@/assets/styles/globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Mini-story App",
-    description: "A supplement for Effortless English learners",
-    keywords: "effortless english, english"
-}
-export default function RootLayout({ children }: Readonly<{children: React.ReactNode}>) {
-    return (
-        <html lang="en">
-            <body className="bg-leon px-xxx text-foreground">
-                <div>{ children }</div>
-            </body>
-        </html>
-    )
+  title: "Mini-story App",
+  description: "A supplement for Effortless English learners",
+  keywords: "effortless english, english",
+};
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,59 @@
-import createLesson from "@/server/actions/create-lesson"
-import getLesson from "@/server/actions/get-lesson"
-import CreateLessonButton from "./components/create-lesson-button"
+import createLesson from "@/server/actions/create-lesson";
+import getLesson from "@/server/actions/get-lesson";
+import CreateLessonButton from "./components/create-lesson-button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export default async function HomePage() {
-    const { error, success }= await getLesson()
-    if(error) throw new Error(error)
-    
-    if (success) {
-        const lineElements = success.map(line => {
-            return (<li key={line.id}>{ line.title }</li>)
-        })
+  const { error, success } = await getLesson();
+  if (error) throw new Error(error);
 
-        return (
-            <div>
-                <h1>HomePage</h1>
+  if (success) {
+    const lineElements = success.map((line) => {
+      return <li key={line.id}>{line.title}</li>;
+    });
 
-                <ul>
-                    { lineElements }
-                </ul>
+    return (
+      <div>
+        <h1>HomePage</h1>
+        <AlertDialog>
+          <AlertDialogTrigger>Open</AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
 
-                <form action={ createLesson }>
-                    <input className="bg-neutral-950 text-neutral-50" type="text" name="title" placeholder="Title" />
-                    <CreateLessonButton />
-                </form>
-            </div>
-        )
-    }
+        <ul>{lineElements}</ul>
+
+        <form action={createLesson}>
+          <input
+            className="bg-neutral-950 text-neutral-50"
+            type="text"
+            name="title"
+            placeholder="Title"
+          />
+          <CreateLessonButton />
+        </form>
+      </div>
+    );
+  }
 }
